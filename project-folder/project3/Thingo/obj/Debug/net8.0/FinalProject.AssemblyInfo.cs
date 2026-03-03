afe4d219c011884b9f9b84a5236048b0e6a50456
fpom1 <jen21017@byui.edu>
2026-03-03 11:44:10 -0700

    bb

--- project-folder/project3/Thingo/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ project-folder/project3/Thingo/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc704a96bcceb163d35b0e74c87adae5e67e0aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ce7e770390bc22ba0e5d2915882c5a8b755c21")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
