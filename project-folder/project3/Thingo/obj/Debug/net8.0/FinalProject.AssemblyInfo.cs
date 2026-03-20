ddeefee34d499a58630ccad75030c261675eddc0
fpom1 <jen21017@byui.edu>
2026-03-20 16:47:22 -0600

    fix
    fixed bug where nav no longer worked.

--- project-folder/project3/Thingo/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ project-folder/project3/Thingo/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db1818d83970b8a28446411a28bd04baafd257e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0adc0800818b7f9f2de2ecfa75f4d5bd45127d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
